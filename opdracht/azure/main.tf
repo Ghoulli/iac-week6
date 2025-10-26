8071de3301181a6f6b391e912e131a7d992df5c8
Ghoulli <58692950+Ghoulli@users.noreply.github.com>
2025-10-26 18:48:03 +0100

    Update main.tf

--- opdracht/azure/main.tf
+++ opdracht/azure/main.tf
@@ -97,7 +97,7 @@ data "template_file" "cloudinit_azure" {
 
   vars = {
     # Changed to read from a local file
-    ssh_key  = file(var.ssh_public_key_path)
+    ssh_key  = var.ssh_public_key_path
     username = var.admin_username
     #private_key = file(pathexpand("~/.ssh/id_rsa_azure"))
   }
